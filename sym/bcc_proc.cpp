2b9586aad315aee640ccde7ad6b0b8131cf15c85
Julio Faracco <jcfaracco@gmail.com>
2025-01-21 15:35:10 -0300

    Fix clang compilation errors due to variable length arrays (#264)
    There are some occurrences of variable length arrays. We need to change
    them to dynamic allocation to avoid runtime memory issues. This commit
    just fix some of those occurrences by using C++ dynamic memory
    allocation.
    
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- sym/bcc_proc.cpp
+++ sym/bcc_proc.cpp
@@ -476,8 +476,8 @@ static bool which_so_in_process(const char* libname, int pid, char* libpath) {
   char endline[4096], *mapname = NULL, *newline;
   char mappings_file[128];
   const size_t search_len = strlen(libname) + strlen("/lib.");
-  char search1[search_len + 1];
-  char search2[search_len + 1];
+  char *search1 = new char[search_len + 1];
+  char *search2 = new char[search_len + 1];
 
   snprintf(mappings_file, sizeof(mappings_file), "/proc/%ld/maps", (long)pid);
   FILE *fp = fopen(mappings_file, "r");
@@ -507,28 +507,36 @@ static bool which_so_in_process(const char* libname, int pid, char* libpath) {
     }
   } while (ret != EOF);
 
+  delete[] search1;
+  delete[] search2;
+
   fclose(fp);
   return found;
 }
 
 char *bcc_procutils_which_so(const char *libname, int pid) {
   const size_t soname_len = strlen(libname) + strlen("lib.so");
-  char soname[soname_len + 1];
+  char *soname = new char[soname_len + 1];
+  char *ret = NULL;
   char libpath[4096];
   int i;
 
-  if (strchr(libname, '/'))
-    return strdup(libname);
+  if (strchr(libname, '/')) {
+    ret = strdup(libname);
+    goto done;
+  }
 
-  if (pid && which_so_in_process(libname, pid, libpath))
-    return strdup(libpath);
+  if (pid && which_so_in_process(libname, pid, libpath)) {
+    ret = strdup(libpath);
+    goto done;
+  }
 
   if (lib_cache_count < 0)
-    return NULL;
+    goto done;
 
   if (!lib_cache_count && load_ld_cache(LD_SO_CACHE) < 0) {
     lib_cache_count = -1;
-    return NULL;
+    goto done;
   }
 
   snprintf(soname, soname_len + 1, "lib%s.so", libname);
@@ -536,10 +544,14 @@ char *bcc_procutils_which_so(const char *libname, int pid) {
   for (i = 0; i < lib_cache_count; ++i) {
     if (!strncmp(lib_cache[i].libname, soname, soname_len) &&
         match_so_flags(lib_cache[i].flags)) {
-      return strdup(lib_cache[i].path);
+      ret = strdup(lib_cache[i].path);
+      goto done;
     }
   }
-  return NULL;
+
+done:
+  delete[] soname;
+  return ret;
 }
 
 void bcc_procutils_free(const char *ptr) {
